693cee074d13e407f659d29bb3ded3576a629c10
Fianda0 <fiandanesegiuseppe000@gmail.com>
2024-05-29 16:07:12 +0200

    Implementazione chiamata Axios con input

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script>
 import AppHeader from "./components/AppHeader.vue";
 import axios from 'axios';
+import store from './data/store.js'
 
 export default {
   components: {
@@ -8,22 +9,48 @@ export default {
   },
   data() {
     return {
-
+      store,
+      ricerca: ''
     }
   },
   methods: {
+    getAxios() {
+      const options = {
+        method: 'GET',
+        url: 'https://api.themoviedb.org/3/search/movie',
+        params: { query: this.ricerca, include_adult: 'false', language: 'en-US', page: '1' },
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2OGY1MzlkNGM1ZjRlYTEyZDEwMjg5NzFjMzU3MTgzOCIsInN1YiI6IjY2NTcyY2MxMmY5MjRlNzQwMDkzYjZiYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zHZRcfaBkWzhLUgDE3w4SNUZH_7KlESmXcfT9M45XyI'
+        }
+      };
+
+      axios
+        .request(options)
+        .then(function (response) {
+          console.log(response.data.results);
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
+    }
 
 
   },
   mounted() {
-
-
   }
+}
 </script>
 
 <template>
 
   <AppHeader />
+
+  <section id="cerca">
+    <input v-model="ricerca" type="text" placeholder="Cerca">
+    <span>{{ ricerca }}</span>
+    <button @click="getAxios()">Invio</button>
+  </section>
 
 </template>
 
---
+++ src/data/store.js
@@ -0,0 +1,9 @@
+import { reactive } from 'vue';
+
+const ilMioOggettone = {
+
+};
+
+const iMieiDati = reactive(ilMioOggettone);
+
+export default iMieiDati;
