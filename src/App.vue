660d913c8e51e579879c89b48efc40e3296d967e
Fianda0 <fiandanesegiuseppe000@gmail.com>
2024-05-29 15:55:56 +0200

    Creazione strutture file

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script>
+import AppHeader from "./components/AppHeader.vue";
+import axios from 'axios';
+
+export default {
+  components: {
+    AppHeader
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+
+  },
+  mounted() {
+
+
+  }
+</script>
+
+<template>
+
+  <AppHeader />
+
+</template>
+
+<style scoped></style>
